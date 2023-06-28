c93d5ddc15fbacf8f0486296d1ba00db09386244
Mik Vattakandy <mik74@colostate.edu>
2023-06-27 23:38:15 -0600

    completed Dice Set

--- src/DiceSet.java
+++ src/DiceSet.java
@@ -12,7 +12,18 @@ public class DiceSet {
             case D6:
                 myval = rnd.nextInt(6) + 1;
             break;
-
+            case D8:
+                myval = rnd.nextInt(8) + 1;
+            break;
+            case D10:
+                myval = rnd.nextInt(10) + 1;
+            break;
+            case D12:
+                myval = rnd.nextInt(12) + 1;
+            break;
+            case D20:
+                myval = rnd.nextInt(20) + 1;
+            break;
         }
 
 
