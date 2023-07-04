1be48eaf8994aa2cc343c11108170cdfa35e93ee
Mik Vattakandy <mik74@colostate.edu>
2023-07-03 21:31:20 -0600

    Part 1 Test 1

--- src/Knight.java
+++ src/Knight.java
@@ -1,5 +1,107 @@
 public class Knight extends MOB {
 
+    private Fortune activeFortune;
+
+    protected final int id;
+
+    protected int xp;
+
+    public Knight(int id, String name, int hp, int armor, int hitmodifier, DiceType damageDie, int xp){
+        super(name, hp, armor, hitmodifier, damageDie);
+        this.id = id;
+        this.xp = xp;
+    }
+
+   public void addXP(int xp){
+        this.xp += xp;
+    }
+
+   public Fortune getActiveFortune(){
+        return this.activeFortune;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+
+    public int getXP(){
+        return this.xp;
+    }
+
+    public void setActiveFortune(Fortune activeFortune){
+        this.activeFortune = activeFortune;
+    }
+
+    public void setXP(int xp){
+        this.xp = xp;
+    }
+
+    public String toString(){
+        String formattedString = "";
+        formattedString += "+======================+\n";
+        formattedString += "|"+getName()+"             |\n";
+        formattedString += "|HP:"+getHP()+"             |\n";
+        formattedString += "|AC:"+getArmor()+"             |\n";
+        formattedString += "|Hit:"+getHitModifier()+"             |\n";
+        formattedString += "|Damage:"+getDamageDie()+"             |\n";
+        formattedString += "|XP:"+getXP()+"             |\n";
+        formattedString += "+======================+\n";
+        return formattedString;
+   }
+
+
+    public int getArmor(){
+        if (getActiveFortune() == null){
+            return super.getArmor();
+        }
+        else{
+            return super.getArmor() + getActiveFortune().getArmor();
+        }
+    }
+
+    public DiceType getDamageDie(){
+        if (getActiveFortune() == null){
+            return super.getDamageDie();
+        }
+        else{
+            return getActiveFortune().getDamageDie();
+        }
+
+        
+    }
+
+    public int getHitModifier(){
+        if (getActiveFortune() == null){
+            return super.getHitModifier();
+        }
+        else{
+            return super.getHitModifier() + getActiveFortune().getHitModifier();
+        }
+    }
+
+    public String toCSV(){
+        String formattedString = "";
+        formattedString += this.id + ",";
+        formattedString += this.getName() + ",";
+        formattedString += this.getHP() + ",";
+        formattedString += this.getArmor() + ",";
+        formattedString += this.getHitModifier() + ",";
+        formattedString += this.getDamageDie() + ",";
+        formattedString += this.getXP() + ",";
+        if (this.getActiveFortune() == null){
+            formattedString += "null";
+        }
+        else{
+            formattedString += this.getActiveFortune().getName();
+        }
+        return formattedString;
+    }
+
+
+
+
+    
+
 
 
     public static void main(String[] args) {
--- src/MOB.java
+++ src/MOB.java
@@ -1,29 +1,73 @@
 public class MOB implements Attributes {
 
+
+    protected int armor;    
+    protected int maxHP;
+    protected int damage;
+    protected DiceType damageDie;
+    protected int hitModifier;
+    private final String name;
+
+    MOB(String name, int hp, int armor, int hitModifier, DiceType damageDie){
+        this.name = name;
+        this.maxHP = hp;
+        this.armor = armor;
+        this.hitModifier = hitModifier;
+        this.damageDie = damageDie;
+    }
+
     @Override
     public int getArmor() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.armor;
     }
 
     @Override
     public int getMaxHP() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.maxHP;
     }
 
     @Override
     public DiceType getDamageDie() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.damageDie;
     }
 
     @Override
     public int getHitModifier() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.hitModifier;
     }
     
+    public void addDamage(int damage){
+        this.damage += damage;
+    }
+
+    public MOB copy(){
+        return new MOB(this.name, this.maxHP, this.armor, this.hitModifier, this.damageDie);
+    }
+
+    public int getHP(){
+        return this.maxHP - this.damage;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void resetDamage(){
+        this.damage = 0;
+    }
+
+    public String toString(){
+        String formattedString = "";
+        formattedString += "+======================+\n";
+        formattedString += "|"+getName()+"             |\n";
+        formattedString += "|HP:"+getHP()+"|\n";
+        formattedString += "|AC:"+getArmor()+"|\n";
+        formattedString += "|Hit:"+getHitModifier()+"|\n";
+        formattedString += "|Damage:"+getDamageDie()+"|\n";
+        formattedString += "+======================+\n";
+        return formattedString;
+    }
+
     public static void main(String[] args) {
         
     }
